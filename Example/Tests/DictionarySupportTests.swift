2422f1fa56d2ae33f28425f972b340be83431f01
Wagner Truppel <wagner@novoda.com>
2017-06-04 17:29:08 +0100

    Commented out support for Dictionary hashing since Swift can't decide between 3 different hashing methods when I think it should be able to. Need to investigate further.

--- Example/Tests/DictionarySupportTests.swift
+++ Example/Tests/DictionarySupportTests.swift
@@ -4,17 +4,93 @@ import WTUniquePrimitiveType
 
 class DictionarySupportTests: XCTestCase {
 
-    func testBoxing() {
-
+    func testBoxingWhenBothKeyAndValueTypesAreUniquePrimitiveTypes() {
+        let dict: [Bool: Double] = [false: 1.0, true: 5.0]
+        let coreTemps: [ReactorConnected: ReactorCoreTemperature] = dict.boxed()
+        let origKeys = Array(dict.keys).sorted()
+        let boxedKeys = coreTemps.keys.map {$0.value}.sorted()
+        XCTAssertEqual(boxedKeys, origKeys)
+        let origVals = Array(dict.values).sorted()
+        let boxedVals = coreTemps.values.map {$0.value}.sorted()
+        XCTAssertEqual(boxedVals, origVals)
     }
 
-    func testUnboxing() {
+    func testUnboxingWhenBothKeyAndValueTypesAreUniquePrimitiveTypes() {
+        let dict1: [Bool: Double] = [false: 1.0, true: 5.0]
+        let coreTemps: [ReactorConnected: ReactorCoreTemperature] = dict1.boxed()
+        let dict2: [Bool: Double] = coreTemps.unboxed()
+        XCTAssertEqual(dict2, dict1)
     }
 
-    func testHashing() {
+//    func testHashingWhenBothKeyAndValueTypesAreUniquePrimitiveTypes() {
+//        let dict: [Bool: Double] = [false: 1.0, true: 5.0]
+//        let coreTemps: [ReactorConnected: ReactorCoreTemperature] = dict.boxed()
+//        let origKeys = Array(dict.keys).sorted()
+//        let origVals = Array(dict.values).sorted()
+//        let expected = zip(origKeys, origVals)
+//            .map { $0.0.hashValue ^ $0.1.hashValue }
+//            .reduce(1.hashValue) { $0 ^ $1 }
+//        XCTAssertEqual(coreTemps.hashValue, expected)
+//    }
 
+    func testBoxingWhenOnlyKeyTypesIsAUniquePrimitiveType() {
+        let dict: [Bool: Double] = [false: 1.0, true: 5.0]
+        let coreTemps: [ReactorConnected: Double] = dict.boxed()
+        let origKeys = Array(dict.keys).sorted()
+        let boxedKeys = coreTemps.keys.map {$0.value}.sorted()
+        XCTAssertEqual(boxedKeys, origKeys)
+        let origVals = Array(dict.values).sorted()
+        let boxedVals = coreTemps.values.map {$0}.sorted()
+        XCTAssertEqual(boxedVals, origVals)
     }
-    
+
+    func testUnboxingWhenOnlyKeyTypesIsAUniquePrimitiveType() {
+        let dict1: [Bool: Double] = [false: 1.0, true: 5.0]
+        let coreTemps: [ReactorConnected: Double] = dict1.boxed()
+        let dict2: [Bool: Double] = coreTemps.unboxed()
+        XCTAssertEqual(dict2, dict1)
+    }
+
+//    func testHashingWhenOnlyKeyTypesIsAUniquePrimitiveType() {
+//        let dict: [Bool: Double] = [false: 1.0, true: 5.0]
+//        let coreTemps: [ReactorConnected: Double] = dict.boxed()
+//        let origKeys = Array(dict.keys).sorted()
+//        let origVals = Array(dict.values).sorted()
+//        let expected = zip(origKeys, origVals)
+//            .map { $0.0.hashValue ^ $0.1.hashValue }
+//            .reduce(1.hashValue) { $0 ^ $1 }
+//        XCTAssertEqual(coreTemps.hashValue, expected)
+//    }
+
+    func testBoxingWhenOnlyValueTypesIsAUniquePrimitiveType() {
+        let dict: [Bool: Double] = [false: 1.0, true: 5.0]
+        let coreTemps: [Bool: ReactorCoreTemperature] = dict.boxed()
+        let origKeys = Array(dict.keys).sorted()
+        let boxedKeys = coreTemps.keys.map {$0}.sorted()
+        XCTAssertEqual(boxedKeys, origKeys)
+        let origVals = Array(dict.values).sorted()
+        let boxedVals = coreTemps.values.map {$0.value}.sorted()
+        XCTAssertEqual(boxedVals, origVals)
+    }
+
+    func testUnboxingWhenOnlyValueTypesIsAUniquePrimitiveType() {
+        let dict1: [Bool: Double] = [false: 1.0, true: 5.0]
+        let coreTemps: [Bool: ReactorCoreTemperature] = dict1.boxed()
+        let dict2: [Bool: Double] = coreTemps.unboxed()
+        XCTAssertEqual(dict2, dict1)
+    }
+
+//    func testHashingWhenOnlyValueTypesIsAUniquePrimitiveType() {
+//        let dict: [Bool: Double] = [false: 1.0, true: 5.0]
+//        let coreTemps: [Bool: ReactorCoreTemperature] = dict.boxed()
+//        let origKeys = Array(dict.keys).sorted()
+//        let origVals = Array(dict.values).sorted()
+//        let expected = zip(origKeys, origVals)
+//            .map { $0.0.hashValue ^ $0.1.hashValue }
+//            .reduce(1.hashValue) { $0 ^ $1 }
+//        XCTAssertEqual(coreTemps.hashValue, expected)
+//    }
+
 }
 
 
