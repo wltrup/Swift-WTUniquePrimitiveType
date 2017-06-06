b2851749e946f02130e5f6fc895562f5038f7caa
Wagner Truppel <wagner@novoda.com>
2017-06-06 11:29:51 +0100

    Added the ability for any UniqueIntegerType-conforming type to initialise itself with an Int (but it may throw) and to retrieve the underlying value as an Int (again, could throw).

--- Example/Tests/UniqueIntegerTypeTests.swift
+++ Example/Tests/UniqueIntegerTypeTests.swift
@@ -10,9 +10,11 @@
 // swiftlint:disable trailing_newline
 // swiftlint:disable file_length
 // swiftlint:disable type_body_length
+// swiftlint:disable variable_name
 
 import XCTest
 import WTUniquePrimitiveType
+
 
 class UniqueIntegerTypeTests: XCTestCase {
 
@@ -25,6 +27,91 @@ class UniqueIntegerTypeTests: XCTestCase {
     func testExpressibleByIntegerLiteral() {
         let userId = UserId(integerLiteral: 100)
         XCTAssertEqual(userId.value, 100)
+    }
+
+    struct Quantity<T: _StringInitialisableInteger>: UniqueIntegerType {
+        public typealias PrimitiveType = T
+        public let value: T
+        public init(_ value: T) {
+            self.value = value
+        }
+    }
+
+    func testIntInit() {
+
+        func applyIntInitTest<T: _StringInitialisableInteger>(_ ival: Int, _ tval: T) {
+            do {
+                let quantity = try Quantity<T>(intValue: ival)
+                XCTAssertEqual(quantity.value, tval)
+            } catch {
+                guard let e = error as? UniquePrimitiveTypeError<Int, T.Type> else {
+                    XCTFail()
+                    return
+                }
+                XCTAssertTrue(e.kind == .initialization)
+                XCTAssertTrue(e.sourceValue == ival)
+            }
+        }
+
+        applyIntInitTest(-100, Int8(-100)) // should pass
+        applyIntInitTest(-1_000, Int8(-100)) // should throw
+
+        applyIntInitTest(-100, Int16(-100)) // should pass
+        applyIntInitTest(-100_000, Int16(-100)) // should throw
+
+        applyIntInitTest(-100, Int32(-100)) // should pass
+        applyIntInitTest(-100_000_000_000, Int32(-100)) // should throw
+
+        applyIntInitTest(-100, Int64(-100)) // should pass
+
+        applyIntInitTest(100, UInt8(100)) // should pass
+        applyIntInitTest(-100, UInt8(100)) // should throw
+        applyIntInitTest(1_000, UInt8(100)) // should throw
+
+        applyIntInitTest(100, UInt16(100)) // should pass
+        applyIntInitTest(-100, UInt16(100)) // should throw
+        applyIntInitTest(100_000, UInt16(100)) // should throw
+
+        applyIntInitTest(100, UInt32(100)) // should pass
+        applyIntInitTest(-100, UInt32(100)) // should throw
+        applyIntInitTest(100_000_000_000, UInt32(100)) // should throw
+
+        applyIntInitTest(100, UInt64(100)) // should pass
+        applyIntInitTest(-100, UInt64(100)) // should throw
+
+    }
+
+    func testValueAsInt() {
+
+        func applyValueAsIntTest<T: _StringInitialisableInteger>(_ tval: T) {
+            do {
+                guard let ival = Int("\(tval)") else { XCTFail(); return }
+                let quantity = Quantity<T>(tval)
+                let intValue = try quantity.valueAsInt()
+                XCTAssertEqual(intValue, ival)
+            } catch { XCTFail() }
+        }
+
+        applyValueAsIntTest(Int8.max)
+        applyValueAsIntTest(Int16.max)
+        applyValueAsIntTest(Int32.max)
+        applyValueAsIntTest(Int64.max)
+        applyValueAsIntTest(UInt8.max)
+        applyValueAsIntTest(UInt16.max)
+        applyValueAsIntTest(UInt32.max)
+
+        do {
+            let quantity = Quantity<UInt64>(UInt64.max)
+            let _ = try quantity.valueAsInt()
+        } catch {
+            guard let e = error as? UniquePrimitiveTypeError<UInt64, Int.Type> else {
+                XCTFail()
+                return
+            }
+            XCTAssertTrue(e.kind == .conversion)
+            XCTAssertTrue(e.sourceValue == UInt64.max)
+        }
+
     }
 
     func testDescription() {
--- WTUniquePrimitiveType/Classes/UniqueIntegerType.swift
+++ WTUniquePrimitiveType/Classes/UniqueIntegerType.swift
@@ -26,4 +26,45 @@ extension UniqueIntegerType where Self.PrimitiveType: ExpressibleByIntegerLitera
     }
 }
 
+// MARK: - Convenience
+
+extension UniqueIntegerType {
+
+    public func valueAsInt() throws -> Int {
+        guard let intValue = Int("\(value)") else {
+            throw UniquePrimitiveTypeError(kind: .conversion,
+                                           sourceValue: value,
+                                           targetType: Int.self)
+        }
+        return intValue
+    }
+    
+}
+
+
+public protocol _StringInitialisableInteger: Integer {
+    init?(_ text: String, radix: Int)
+}
+extension Int8: _StringInitialisableInteger {}
+extension Int16: _StringInitialisableInteger {}
+extension Int32: _StringInitialisableInteger {}
+extension Int64: _StringInitialisableInteger {}
+extension UInt8: _StringInitialisableInteger {}
+extension UInt16: _StringInitialisableInteger {}
+extension UInt32: _StringInitialisableInteger {}
+extension UInt64: _StringInitialisableInteger {}
+
+extension UniqueIntegerType where Self.PrimitiveType: _StringInitialisableInteger {
+
+    public init(intValue value: Int) throws {
+        guard let primitiveValue = Self.PrimitiveType.init("\(value)", radix: 10) else {
+            throw UniquePrimitiveTypeError(kind: .initialization,
+                                           sourceValue: value,
+                                           targetType: Self.PrimitiveType.self)
+        }
+        self.init(primitiveValue)
+    }
+
+}
+
 
